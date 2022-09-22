ab41684a14af140937d634d113f0fbbfe1e12765
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-22 04:27:36 -0400

    first contract and initialize it

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig, task} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
