90b60a4026f8e39ccf6f7d2d817f2424a0ec9a4e
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-22 04:31:18 -0400

    add @ts-ignore

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,4 +1,5 @@
 const main = async () => {
+	// @ts-ignore
 	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 	const waveContract = await waveContractFactory.deploy();
 	await waveContract.deployed();
