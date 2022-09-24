89e3ad3d04ab84baa16e80ee63b7c987b765b1a2
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-24 00:45:26 -0400

    connection to metamask wallet and connect button

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,9 +1,48 @@
-<script>
-  name = "Soushi888";
+<script lang="ts">
+  import {onMount} from "svelte";
+  import ConnectWallet from "./ConnectWallet.svelte";
 
-  function wave() {
-    alert(`${name} just waved!`);
+  const {ethereum} = window;
+  let status: string, isLogged: boolean, account: Account;
+
+  const checkIfWalletIsConnected = async () => {
+    if (ethereum) {
+      status = "Metamask is installed";
+    } else {
+      status = "Metamask is not installed";
+      return;
+    }
+
+    const accounts = await ethereum.request({method: 'eth_accounts'});
+    console.log(accounts);
+
+    if (accounts.length !== 0) {
+      account = accounts[0];
+      isLogged = true;
+    } else {
+      isLogged = false;
+      console.log("Wallet is not connected");
+    }
+  };
+
+  $: isLogged ? status = "Connected" : status = "Not connected";
+
+  const connectWallet = async () => {
+    const accounts = await ethereum.request({method: "eth_requestAccounts"});
+    account = accounts[0];
+
+    if (account) {
+      isLogged = true;
+    }
+  };
+
+  const wave = () => {
+    alert(`${account} just waved!!`);
   }
+
+  onMount(() => {
+    checkIfWalletIsConnected();
+  });
 </script>
 
 <main>
@@ -14,13 +53,28 @@
         👋 Hey there!
       </div>
 
-      <div class="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+      <div class="status">
+        <p class={isLogged ? "success" : "fail"}>{status}</p>
       </div>
 
-      <button class="waveButton" on:click={wave}>
-        Wave at Me
-      </button>
+      <div class="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave
+        at me!
+      </div>
+
+      {#if (ethereum)}
+        <div class="connect-btn">
+          <button id="connect-btn" on:click={connectWallet}>Connect Wallet</button>
+        </div>
+      {/if}
+
+      {#if (isLogged)}
+        <div class="wave-btn">
+          <button on:click={wave}>
+            Wave at Me
+          </button>
+        </div>
+      {/if}
     </div>
   </div>
 </main>
@@ -52,11 +106,40 @@
     margin-top: 16px;
   }
 
-  .waveButton {
+  .wave-btn {
+    display: flex;
+    justify-content: center;
+    margin-top: 16px;
+  }
+
+  .wave-btn button {
     cursor: pointer;
     margin-top: 16px;
-    padding: 8px;
-    border: 0;
+    padding: 10px 20px;
+    border: none;
     border-radius: 5px;
+  }
+
+  .connect-btn {
+    display: flex;
+    justify-content: center;
+    margin-top: 16px;
+  }
+
+  .connect-btn button {
+    background-color: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .success {
+    color: green;
+  }
+
+  .fail {
+    color: red;
   }
 </style>
---
+++ ui/src/ConnectWallet.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<button id="connect-btn">Connect Wallet</button>
+
+<style>
+  #connect-btn {
+    background-color: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+</style>
